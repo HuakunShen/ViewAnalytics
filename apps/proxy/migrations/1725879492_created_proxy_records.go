ea126fab7d760987f8deefe9bc5469cf254d4442
Huakun Shen <huakun.shen@huakunshen.com>
2024-09-09 07:00:08 -0400

    Update proxy db migration script

---
+++ apps/proxy/migrations/1725879492_created_proxy_records.go
@@ -0,0 +1,76 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		jsonData := `{
+			"id": "g9evvmsbp5k8552",
+			"created": "2024-09-09 10:58:12.416Z",
+			"updated": "2024-09-09 10:58:12.416Z",
+			"name": "proxy_records",
+			"type": "base",
+			"system": false,
+			"schema": [
+				{
+					"system": false,
+					"id": "sn3t3dey",
+					"name": "url",
+					"type": "text",
+					"required": true,
+					"presentable": false,
+					"unique": false,
+					"options": {
+						"min": null,
+						"max": null,
+						"pattern": ""
+					}
+				},
+				{
+					"system": false,
+					"id": "fzcvhk2o",
+					"name": "ip",
+					"type": "text",
+					"required": true,
+					"presentable": false,
+					"unique": false,
+					"options": {
+						"min": null,
+						"max": null,
+						"pattern": ""
+					}
+				}
+			],
+			"indexes": [],
+			"listRule": null,
+			"viewRule": null,
+			"createRule": null,
+			"updateRule": null,
+			"deleteRule": null,
+			"options": {}
+		}`
+
+		collection := &models.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return daos.New(db).SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("g9evvmsbp5k8552")
+		if err != nil {
+			return err
+		}
+
+		return dao.DeleteCollection(collection)
+	})
+}
